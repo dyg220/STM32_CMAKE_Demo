3a6cc738723a6131d5445b50ec1193f0d5f8dc0a
dyg220 <1503639251@qq.com>
2025-07-20 23:52:34 +0800

    add

--- User/src/usart1.c
+++ User/src/usart1.c
@@ -164,7 +164,7 @@ int fputc(int ch, FILE* f)
 PUTCHAR_PROTOTYPE
 {
 	USART_SendData(USART1, (uint8_t)ch);
-	while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
 	return ch;
 }
 
